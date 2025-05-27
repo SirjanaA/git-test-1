9e2ab0347e1ef9cde966a7ff7fd70a5ef5e9a654
SirjanaA <alesrizu92@icloud.com>
2025-05-27 15:16:29 +0000

    new dockerfile added

---
+++ jenkins-docker/docker_example/executors.groovy
@@ -0,0 +1,8 @@
+import jenkins.model.*
+Jenkins.instance.setNumExecutors(0) 
+
+# Recommended to not run builds on the built-in node
+
+
+# From the official jenkins github documentation
+# https://github.com/jenkinsci/docker
